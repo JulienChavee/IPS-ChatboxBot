e3281598164b9dfb13536b5f9b445dc820ebf892
Julien Chavée <julien@MacBook-Pro-de-Julien.local>
2017-05-25 19:42:43 +0200

    Fix #2

---
+++ p03d5651/hooks/7b686cf29106cf697196948d92da43bb.php
@@ -0,0 +1,32 @@
+//<?php
+
+/* To prevent PHP errors (extending class does not exist) revealing path */
+if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
+{
+	exit;
+}
+
+class hook43 extends _HOOK_CLASS_
+{
+
+
+	/**
+	 * Execute
+	 *
+	 * @return	void
+	 */
+	public function execute()
+	{
+		call_user_func_array( 'parent::execute', func_get_args() );
+		if($id = \IPS\Db::i()->select( 'plugin_id', 'core_plugins', array( 'plugin_name=?', 'Chatbox Tweet Import' ) )->first())
+		{
+			\IPS\Output::i()->sidebar['actions']['extender'] = [
+				'icon'  => 'bullhorn',
+				'link'  => \IPS\Http\Url::internal( "app=core&module=applications&controller=plugins&do=settings&id={$id}" ),
+				'title' => 'Chatbox Tweet Import',
+				'color' => 'ipsButton_positive'
+			];
+		}
+	}
+
+}
