dc1aca11b8dffa563a7c76bcf3851a8b2deb95da
Julien Chavée <julien@MacBook-Pro-de-Julien.local>
2017-05-25 19:35:37 +0200

    Fix #1

--- p03d5651/hooks/14395386f18737bf26e7e733ca75b830.php
+++ p03d5651/hooks/14395386f18737bf26e7e733ca75b830.php
@@ -9,33 +9,36 @@ if ( !defined( '\IPS\SUITE_UNIQUE_KEY' ) )
 class hook15 extends _HOOK_CLASS_
 {
 	/**
-	 * Process created object AFTER the object has been created
+	 * Save Changed Columns
 	 *
-	 * @param	\IPS\Content\Comment|NULL	$comment	The first comment
-	 * @param	array						$values		Values from form
 	 * @return	void
 	 */
-	protected function processAfterCreate( $comment, $values )
+	public function save()
 	{
-      $forum = $this->container();
-      $forumsAccepted = json_decode( \IPS\Settings::i()->forum_to_shoutbox );
-      if( in_array( $forum->id, $forumsAccepted ) ) {
-      	$time = time();
-      	$user = \IPS\Settings::i()->tweet_user_post;
-      	$author = $this->author();
-        $authorGroup = \IPS\Member\Group::load( $author->member_group_id );
-      	$authorUrl = preg_replace( '#^https?://#', '//', $author->url() );
-      	$topicUrl = preg_replace( '#^https?://#', '//', $this->url() );
-      	$chat = '<a href="'.$authorUrl.'">'.$authorGroup->formatName(  $author->name ).'</a> a posté un nouveau sujet : <a href="'.$topicUrl.'">'.$this->title.'</a>';
-      
-      	\IPS\Db::i()->insert( 'bimchatbox_chat', array(
-								'user'		=> $user,
-								'chat'		=> $chat,
-								'time'		=> $time,
-							) );
-      }
-      
-		return call_user_func_array( 'parent::processAfterCreate', func_get_args() );
+		call_user_func_array( 'parent::save', func_get_args() );
+
+		if(get_class($this)=='IPS\forums\Topic')
+		{
+			$forum = $this->container();
+			$forumsAccepted = json_decode( \IPS\Settings::i()->forum_to_shoutbox );
+
+			if( in_array( $forum->id, $forumsAccepted ) ) {
+				$time = time();
+				$user = \IPS\Settings::i()->tweet_user_post;
+				$author = $this->author();
+				$authorGroup = \IPS\Member\Group::load( $author->member_group_id );
+				$authorUrl = preg_replace( '#^https?://#', '//', $author->url() );
+				$topicUrl = preg_replace( '#^https?://#', '//', $this->url() );
+				$chat = '<a href="'.$authorUrl.'">'.$authorGroup->formatName(  $author->name ).'</a> a posté un nouveau sujet : <a href="'.$topicUrl.'">'.$this->title.'</a>';
+
+				\IPS\Db::i()->insert( 'bimchatbox_chat', array(
+										'user'		=> $user,
+										'chat'		=> $chat,
+										'time'		=> $time,
+				) );
+			}
+		} else
+			return;
 	}
 
 }
